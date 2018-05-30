84a3eacc5ea3f19cd1bbc353373a9fcdc6bccd4d
unknown <jtc7537@gmail.com>
2018-05-29 21:49:47 -0400

    react router

---
+++ client/src/pages/homepage/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    };
+
+    render() {
+        return(
+            <div>
+                <p>This is the homepage</p>
+            </div>
+        )
+    };
+};
+
+export default Home;
