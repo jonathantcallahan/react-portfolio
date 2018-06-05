c4003aac665ee5c00d61a9327a5d2a265490d17d
unknown <jtc7537@gmail.com>
2018-06-04 23:34:34 -0400

    menu

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -15,23 +15,28 @@ class Header extends React.Component {
 
     render() {
         return(
-            <div className='header'>
-                <div id='header-name'>
-                    JONATHAN
-                </div>
-                <div className='header-three'>
-                    <div className='header-el'>CONTACT</div>
-                    <div className='header-el'>PROJECTS</div>
-                    <div className='header-el'>ABOUT ME</div>
-                    
-                    <div id='veggieburger' onClick={() => this.toggleHeader()}>
-                        <div className='line'></div>
-                        <div className='line'></div>
-                        <div className='line'></div>
+            <div>
+                <div className='header'>
+                    <div id='header-name'>
+                        JONATHAN
+                    </div>
+                    <div className='header-three'>
+                        <div className='header-el'>CONTACT</div>
+                        <div className='header-el'>PROJECTS</div>
+                        <div className='header-el'>ABOUT ME</div>
+                        
+                        <div id='veggieburger' onClick={() => this.toggleHeader()}>
+                            <div className='line'></div>
+                            <div className='line'></div>
+                            <div className='line'></div>
+                        </div>
                     </div>
                 </div>
                 <div id='smu' className={this.state.menu}>
                     <div id='menuhead'>MENU</div>
+                    <div className='sideitem'>CONTACT</div>
+                    <div className='sideitem'>PROJECTS</div>
+                    <div className='sideitem'>ABOUT ME</div>
                 </div>
             </div>
         )
