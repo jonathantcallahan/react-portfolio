8efc1f96a5a00205d55d7e28df812c8645151a0e
unknown <jtc7537@gmail.com>
2018-06-04 23:11:11 -0400

    mobile menu

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -8,16 +8,27 @@ class Header extends React.Component {
 
         };
     };
+
+    toggleHeader = () => {
+
+    };
+
     render() {
         return(
             <div className='header'>
-                <div id='header-name' className='header-el'>
+                <div id='header-name'>
                     JONATHAN
                 </div>
                 <div className='header-three'>
                     <div className='header-el'>CONTACT</div>
                     <div className='header-el'>PROJECTS</div>
                     <div className='header-el'>ABOUT ME</div>
+                    
+                    <div id='veggieburger'>
+                        <div className='line'></div>
+                        <div className='line'></div>
+                        <div className='line'></div>
+                    </div>    
                 </div>
             </div>
         )
