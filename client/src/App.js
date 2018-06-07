86c3f77720a4de47b1985c0cb2d6fba71861ebaf
unknown <jtc7537@gmail.com>
2018-06-06 21:33:36 -0400

    parallax

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
-
+import { ParallaxProvider } from 'react-scroll-parallax';
 import Homepage from './pages/homepage/Homepage'
 
 class App extends Component {
   render() {
     return (
+    <ParallaxProvider>
       <Router>
         <div id='main-body'>
           <Route exact path='/' component={Homepage}/>
         </div>
       </Router>
+    </ParallaxProvider>
     );
   }
 }
--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './../../components/header/Header';
 import './Homepage.css';
+import { ParallaxBanner } from 'react-scroll-parallax';
 
 class Home extends React.Component {
 
@@ -15,8 +16,26 @@ class Home extends React.Component {
         return(
             <div>
                 <Header />
-                <div className='fullpage red'>
-                </div>
+                    <ParallaxBanner
+                        className="your-class"
+                        layers={[
+                            {
+                                image: 'https://bonexpose.com/wp-content/uploads/2015/06/Contact-Us.jpg',
+                                amount: 0.2,
+                                slowerScrollRate: true,
+                            },{
+                                image: 'http://www.macosxtips.co.uk/geeklets/modules/upload/attachments/cutout.png',
+                                amount: 0.5,
+                                slowerScrollRate: false,
+                            },
+                        ]}
+                        style={{
+                            height: '100vh',
+                        }}
+                    >
+                        <h1>Banner Children</h1>
+                    </ParallaxBanner>
+                <div className='fullpage'></div>
                 </div>
         )
     };
